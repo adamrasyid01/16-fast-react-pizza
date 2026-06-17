a4156488726f2b1c378fc571a3b89a77fed0285b
Adam Rasyid N <adamrasyid01@gmail.com>
2026-06-17 14:36:39 +0700

    Feat(order) : Fetching Orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateUser from "./features/user/CreateUser";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/create-user",
+        element: <CreateUser />,
+      },
+    ],
+  },
+]);
 export default function App() {
-  return <div>hloooo</div>;
+  return <RouterProvider router={router} />;
 }
