bd7c7f2115cf2311aef175929be6b9fb15c29c85
Adam Rasyid N <adamrasyid01@gmail.com>
2026-06-19 16:52:10 +0700

    Styling the Form etc

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,12 +1,12 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
   console.log(menu);
   return (
-    <ul>
+    <ul className="divide-stone-2 divide-y py-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
